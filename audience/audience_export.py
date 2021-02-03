18dd4f7be25b65c0cc255f9a3bd4f59dc9cbc8a5
cw257900 <cw2579@gmail.com>
2021-02-03 01:01:38 -0600

    updated code to commit

--- audience/audience_export.py
+++ audience/audience_export.py
@@ -1,19 +1,17 @@
-
 class Audience:
-
     segment = "test segment 1"
     campaign = "test campaign 1"
 
     def __init__(self, segment):
+        print ("from __init")
         self.segment = segment
 
-    def export_audience (self):
-        print ("Audience:export_audience " + self.segment + "   " + self.campaign )
+    def export_audience(self):
+        print ("Audience:export_audience " + self.segment + "   " + self.campaign)
 
 
-
-
-
-aud = Audience ("inital segment")
-print ("main function " + aud.segment + "   " + aud.campaign)
-aud.export_audience()
+if __name__ == '__main__':
+    print ("from main")
+    aud = Audience("inital segment")
+    print ("main function release branch " + aud.segment + "   " + aud.campaign)
+    aud.export_audience()
